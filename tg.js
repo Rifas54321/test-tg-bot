61465257465973d880ce1ec921d4b68e27d53c12
Rifas54321 <rifasalappuzhakl04@gmail.com>
2023-10-24 17:10:54 +0530

    Add files via upload

---
+++ tg.js
@@ -0,0 +1,18 @@
+const TelegramBot = require("node-telegram-bot-api");
+const { Hercai } = require('hercai');
+const herc = new Hercai();
+const token = "6608460313:AAEtxzs2Bh2kb5f1aopKF_L98mGJBG8oOX4"
+const bot = new TelegramBot(token,{polling:true})
+bot.on("message",(msg)=>{
+  const chatId = msg.chat.id;
+  const msgText = msg.text;
+  console.log(msg.text);
+  if (msgText === "/start"){
+    bot.sendMessage(chatId,"Welcome to rfs ai")
+  }else{
+  herc.question({model:"v2",content:msgText}).then(response => {
+bot.sendMessage(chatId,response.reply);
+
+});
+   }
+});
