87c7df89ffaf6e6b52f796aef8872ce73f72345f
Rifas54321 <rifasalappuzhakl04@gmail.com>
2023-10-25 23:39:02 +0530

    update tg.js

--- tg.js
+++ tg.js
@@ -1,16 +1,16 @@
-const TelegramBot = require("node-telegram-bot-api");
+cconst TelegramBot = require("node-telegram-bot-api");
 const { Hercai } = require('hercai');
 const herc = new Hercai();
 const token = "6608460313:AAEtxzs2Bh2kb5f1aopKF_L98mGJBG8oOX4"
 const bot = new TelegramBot(token,{polling:true})
-bot.on("message",(msg)=>{
+bot.on("message",async (msg)=>{
   const chatId = msg.chat.id;
   const msgText = msg.text;
   console.log(msg.text);
   if (msgText === "/start"){
     bot.sendMessage(chatId,"Welcome to rfs ai")
   }else{
-  herc.question({model:"v2",content:msgText}).then(response => {
+  await herc.question({model:"v2",content:msgText}).then(response => {
 bot.sendMessage(chatId,response.reply);
 
 });
