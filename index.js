95eeb986bdda95b9deb8bd86909b27846273a1b8
Rifas54321 <rifasalappuzhakl04@gmail.com>
2023-11-13 14:21:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const teleGram = require("node-telegram-bot-api");
 const {Hercai} = require("hercai");
-const bot_token ="6507681162:AAGaoadFk7_24D2TD7MItN7yawuxOkKMe3A";
+const bot_token ="6608460313:AAEtxzs2Bh2kb5f1aopKF_L98mGJBG8oOX4";
 const bot = new teleGram(bot_token,{polling:true})
 const hercai = new Hercai();
 bot.on("message",async(msg)=>{
