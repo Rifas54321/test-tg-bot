c386c7d3ade4851695b764d0801d94f0fcd599d0
Rifas54321 <rifasalappuzhakl04@gmail.com>
2023-11-25 19:18:30 +0530

    update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const teleGram = require("node-telegram-bot-api");
 const express = require("express")
 const app = express();
+app.use(expressurlencoded({extended:true}))
+app.use(express.json());
 app.get("/",async(req,res)=>{
   res.send('<h2 style="text-align:center;">running</h2>')
 })
@@ -34,21 +36,31 @@ bot.onText(/\/image(.+)/, async(msg, match) => {
  const chatId = msg.chat.id;
  const img_resp = match[1];  
  console.log("image : ",img_resp)
+ try{
    hercai.drawImage({model:"v2",prompt:img_resp}).then(response => {
   bot.sendChatAction(chatId,"typing")
    bot.sendPhoto(chatId,Photo=response.url)
  })
+ }catch{
+   bot.sendChatAction(chatId,"typing");
+   bot.sendMessage(chatId,"sorry! error occured please try again later")
+ }
  });
  
  bot.onText(/\/text(.+)/, async(msg, match) => {
  const chatId = msg.chat.id;
  const text_resp = match[1];  
  console.log("text : ",text_resp)
+try{
 hercai.question({model:"v2",content:text_resp}).then(response => {
   bot.sendChatAction(chatId,"typing")
   bot.sendMessage(chatId,response.reply)
 
 });
+catch{
+  bot.sendChatAction(chatId,"sorry! error occured please try again
+  later")
+}
 
  });
 app.listen(3100,()=>{console.log("server started")})
